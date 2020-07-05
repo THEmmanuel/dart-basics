fdb1b452444d35514f5ff20fc9a1dca69928e891
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-05 16:48:38 +0100

    Comments

---
+++ comments.dart
@@ -0,0 +1,8 @@
+//This is a regular, single line comment
+
+///This is a documentation document, used to document libraries.
+///Starts a new line automagically
+
+/*
+This is a regular multi line commment
+ */ 
