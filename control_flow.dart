4b35fd96cc336ebf0a5bf37884cd29ddb9967528
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-05 16:46:10 +0100

    functions

--- control_flow.dart
+++ control_flow.dart
@@ -1,9 +1,17 @@
 var age = 20;
+var someWords = ['Cars', 'Belt', 'Television', 'Jump'];
 
 void main() {
+  // if else
   if (age >= 18) {
     print("Hey, you're an adult now!");
   } else if (age < 18) {
     print('Heyo, minor!');
   }
+
+  //For
+  for (var word in someWords){
+    print(word);
+  }
 }
+
---
+++ fucntions.dart
@@ -0,0 +1,12 @@
+//It's recommended to specify the function's return types.
+
+void main() {
+  int addition(int numberA, int numberB) {
+    int result = numberA + numberB;
+    return result;
+  }
+
+  var additionResult = addition(15, 25);
+
+  print(additionResult);
+}
