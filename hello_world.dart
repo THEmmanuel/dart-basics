46198be097422cfea4db823b4994b3425392f636
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-07-05 15:58:16 +0100

    Variable Basics!

--- hello_world.dart
+++ hello_world.dart
@@ -2,3 +2,5 @@ void main(){
   print ('hello world!');
 }
 
+//The main() function is a special, required function where the program execution starts
+//The print() function simply logs it's arguments to the console.
---
+++ variables.dart
@@ -0,0 +1,10 @@
+//Thanks to type inference, mos variables do not need an explicit type declaration
+
+var name = 'Emmanuel'; //String
+//unlike Javascript the semi-colon is necessary.
+var year = 2000; //Integer
+var height = 6.0;
+var hobbies = ['Reading', 'Gaming', 'Drawing', 'Coding'];
+var otherInfo = {
+  'nothing' : 'Literally nothing else to add'
+};
